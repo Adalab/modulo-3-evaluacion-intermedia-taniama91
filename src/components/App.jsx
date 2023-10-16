f481bf1fd94da2c65670b83c9d73d85da41fb538
Tania <taniie_bcn@hotmail.es>
2023-10-16 11:08:17 +0200

    added readme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,44 @@
 //impots dependencias, imagenes, componentes, estilos
-import '../styles/App.scss'
+import {useEffect, useState} from 'react';
+import '../styles/App.scss';
+
 
 
 function App() {
   //funciones, variables, handles
+  const [countryList, setCountryList] = useState([]);
+  const [countrySearch, setCountrySearch] = useState ("")
+
+  useEffect(()=>{
+  fetch ("https://restcountries.com/v3.1/all?fields=name,capital,flag,continents")
+  .then ((response)=>response.json())
+  .then ((data)=>{
+    setCountryList(data)
+    })
+  },[])
   
+  const handleImputSearch = (ev)=>{
+  setCountrySearch(ev.target.value)
+  }
+
+  const renderList = ()=>{
+    return countryList.filter((eachCountry)=> 
+    eachCountry.name.official.toLowerCase().includes(countrySearch.toLowerCase()))
+    
+    
+    .map((eachCountry, i)=>(<li key={i} className='country__item'>
+      {/* bandera */}
+      <p>{eachCountry.flag}</p> 
+      {/* pais */}
+      <p>{eachCountry.name.official}</p>
+      {/* capital */}
+      <p>{eachCountry.capital}</p>
+      {/* continente */}
+      <p>{eachCountry.continents}</p>
+    </li>))
+  }
+
+
   //estructura html
   return (
     <div>
@@ -16,22 +50,26 @@ function App() {
            <form>
             <section className='form__filter'>
               <h2>Filters</h2>
+              <p>By Country</p>
               <input 
               className='form__filter-country'
               autoComplete='off'
               type="search"
               name='country'
               placeholder='Spain'
-              // value={}
-              // onChange={} 
+              value={countrySearch}
+              onChange={handleImputSearch} 
             />
-          <input 
-              className=''
-              type="select"
-              name='country'
-              // value={}
-              // onChange={} 
-            />
+            <p>By Continent</p>
+          <select>
+            <option value="All"></option>
+            <option value="Africa"></option>
+            <option value="North America"></option>
+            <option value="South America"></option>
+            <option value="Europe"></option>
+            <option value="Asia"></option>
+            <option value="Oceania"></option>
+          </select>
           </section>
           <section className='form__add'>
             <h2>Add Country</h2>
@@ -75,6 +113,7 @@ function App() {
           </section>
           </form>
           <ul className="list">
+            {renderList()}
         </ul>
         </main>
      
